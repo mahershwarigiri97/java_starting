013effba6b45e9d6c0b806d5dbbe1bb99d4a98d2
mahershwarigiri97 <maheshwarigiri97@gmail.com>
2026-03-25 00:08:50 +0530

    ATMin progress

---
+++ ATM.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(){
+     
+        Scanner scanner=new Scanner(System.in);
+        double balance=5000;
+         
+
+        while (true) {
+            System.out.println("\nWelcome to the ATM ");
+            System.out.println("1. check balance");
+            System.out.println("2. deposit");
+            System.out.println("3. withdraw");
+            System.out.println("4. exit");
+            System.out.print("choose an option:");
+
+            int choice = scanner.nextInt();
+            
+
+        }
+
+    }
+}
