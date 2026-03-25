5645653d0b53fb6a182e9edd67096bdd43c569ef
mahershwarigiri97 <maheshwarigiri97@gmail.com>
2026-03-25 23:46:47 +0530

    ATM project is done

--- ATM.java
+++ ATM.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ATM {
     public static void main(){
-     
+        
         Scanner scanner=new Scanner(System.in);
         double balance=5000;
          
@@ -17,6 +17,35 @@ public class ATM {
 
             int choice = scanner.nextInt();
             
+            switch (choice) {
+                case 1:
+                    System.out.println("your balance is:"+balance);
+                    break;
+                case 2:
+                    System.out.println("enter amount to deposit:");
+                    double deposit = scanner.nextDouble();
+                    balance += deposit;
+                    System.out.println("Deposited:"+deposit);
+                    break;
+                case 3:
+                    System.out.println("enter amount tom withdraw :");
+                    double withdraw = scanner.nextDouble();
+                    if(withdraw <= balance){
+                        balance-=withdraw;
+                        System.out.println("withdrew:"+withdraw);
+                    } else{
+                        System.out.println("insufficient funds!");
+
+                        }
+                      break;
+                case 4 :
+                    System.out.println("thank you for using the ATM. Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("invalid option . please tryb again.");
+
+            }
 
         }
 
